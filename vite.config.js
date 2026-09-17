9b3942d17c3ee2299e7c5fa9d64bd969fcdd9908
Khamza Dobriev <XDobriev@yandex.ru>
2026-09-17 12:50:20 +0300

    feat: deploy the site to github pages via github actions

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/rsschool-landing-page/',
   build: {
     sourcemap: true,
     rollupOptions: {
